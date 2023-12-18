20a3f4ebf240882fd920e72845d0559cc7bfc6e0
aschmitt 42 <alban.schmitt@hotmail.com>
2023-12-18 17:16:08 +0100

    probleme path

--- src/aux2.c
+++ src/aux2.c
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   aux2.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aschmitt <aschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/07 13:33:58 by aschmitt          #+#    #+#             */
-/*   Updated: 2023/12/18 16:44:43 by aschmitt         ###   ########.fr       */
+/*   Created: 2023/12/18 16:39:52 by aschmitt          #+#    #+#             */
+/*   Updated: 2023/12/18 16:44:31 by aschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-# include <stdio.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <sys/stat.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdlib.h>
+char	*ft_copy(char *cmd)
+{
+	int		i;
+	char	*bin;
 
-char	**ft_split(char const *s, char c);
-char	*find_bin(char *cmd, char **envp);
-void	free_tab(char **path);
-void	error(char *s);
-size_t	ft_strlen(char *s);
-char	*ft_copy(char *cmd);
-
-#endif
+	bin = malloc(sizeof(char) * ft_strlen(cmd) + 1);
+	i = -1;
+	while (cmd[++i])
+		bin[i] = cmd[i];
+	bin[i] = 0;
+	return (bin);
+}
--- src/pipex.c
+++ src/pipex.c
@@ -6,11 +6,35 @@
 /*   By: aschmitt <aschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 13:23:16 by aschmitt          #+#    #+#             */
-/*   Updated: 2023/12/18 15:15:49 by aschmitt         ###   ########.fr       */
+/*   Updated: 2023/12/18 16:55:15 by aschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+char	*args_path(char *args)
+{
+	int		i;
+	int 	a;
+	char	*s;
+
+	i = -1;
+	a = 0;
+	while (args[++i])
+	{
+		if (args[i] == '/')
+			a = i;
+	}
+	s = malloc(sizeof(char) * (i - a));
+	if (!s)
+		return (NULL);
+	i = -1;
+	while (args[++a])
+		s[++i] = args[a];
+	s[++i] = 0;
+	free (args);
+	return (s);
+}
 
 void	command(char *cmd, char **envp)
 {
@@ -20,11 +44,17 @@ void	command(char *cmd, char **envp)
 	args = ft_split(cmd, ' ');
 	if (!args)
 		error("Error malloc\n");
-	bin = find_bin(args[0], envp);
+	if (cmd[0] == '/')
+	{
+		bin = ft_copy(args[0]);
+		args[0] = args_path(args[0]);
+	}
+	else 
+		bin = find_bin(args[0], envp);
 	if (!bin)
 	{
 		free_tab(args);
-		error("Error bin\n");
+		error("Error Path\n");
 	}
 	if (execve(bin, args, envp) == -1)
 		error("Error execution\n");
